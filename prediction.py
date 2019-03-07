481229d1ab88c57826aa80be97036afffef35a2d
HC <45563110+hechaonjau@users.noreply.github.com>
2019-03-07 15:53:37 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -8,12 +8,17 @@ from train import photo, loadPicture, texture_detect
 
 
 # 预测
-def prediction():
+def prediction(n,path,path1):
+    """
+    n:验证图片数量
+    path：验证数据集路径
+    path1:处理后的数据集存放路径
+    """
     train_hist = torch.load('train.mat')
     train_label = torch.load('label.mat')
-    photo(6, '1/', 'hist/')
-    mdata, mlabel = loadPicture(6, 'hist/')
-    hist = texture_detect(6, 1, mdata)
+    photo(n, path, path1)
+    mdata, mlabel = loadPicture(n, path1)
+    hist = texture_detect(n, 1, mdata)
     svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
     model = torch.load('1.pkl')
     clf = model.fit(train_hist, train_label)
